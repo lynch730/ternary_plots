7b0d0097c3685eaa1cc1a9a703935d0e59df21d6
lynch4815 <lynch4815@gmail.com>
2021-05-30 17:09:04 -0500

    Added empty plot functions, renamed directories to be more clear about contents. Need to address names of line_plots, seperating ternary_plot used as lines from ternary_line_plot which is meant only for grid lines

--- add_ternary_paths.m
+++ add_ternary_paths.m
@@ -1,6 +1,6 @@
 function add_ternary_paths
-addpath('base_axes_creation/');
-addpath('ternary_data_plot/');
+addpath('ternary_axes_creation/');
+addpath('ternary_data_plots/');
 addpath('ternary_problem_setup/');
 addpath('utilities/');
 end
