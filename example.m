52f2da5fe4cf35e8b769a0b0444f1e7c71b53769
lynch4815 <lynch4815@gmail.com>
2021-05-30 17:18:49 -0500

    Swapped shell functions for 3d equiv. for clarity, added scatter 3 as potential alternate to surf

--- example.m
+++ example.m
@@ -37,6 +37,6 @@ wlimits = ternary_axes_limits( 100,'l',23,'low',...
      handle = ternary_axes( vgen, vout, vgrid, vtick, vlab );
       
 %% (5) Add ternary data to the plot
-%     ax = ternary_surf(ax, Z, wlimits );
+     handle = ternary_surf( handle, Z, wlimits );
  
  
--- ternary_data_plots/ternary_plot3.m
+++ ternary_data_plots/ternary_plot3.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = ternary_plot(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = ternary_plot3(inputArg1,inputArg2)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
